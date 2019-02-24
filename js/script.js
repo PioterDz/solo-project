258df1404bcc64a64dd140a6abce149b5352b006
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-02-24 13:52:53 +0100

    make quit and banners modals

--- js/script.js
+++ js/script.js
@@ -1,14 +1,45 @@
+// MENU SHOW
+
 function toggleMenu(visible) {
-    document.querySelector('.menu').classList.toggle('show', visible)
+    document.querySelector('.menu').classList.toggle('show', visible);
 }
 
 document.querySelector('.hamburger').addEventListener('click', function(e) {
     e.preventDefault();
-    toggleMenu()
+    toggleMenu();
 });
 
-function quitWindow() {
-    addEventListener.classList()
-    var popup = document.getElementById('quitPopup');
-    popup-quit.classList.toggle("show-popup");
+// QUIT MODAL
+
+var quitModal = document.getElementById('quitModal');
+var quitBtn = document.getElementById('quitBtn');
+
+quitBtn.onclick = function () {
+    quitModal.style.display = "block";
+}
+
+window.onclick = function(event) {
+    if (event.target == quitModal) {
+        quitModal.style.display = "none";
+    }
+}
+
+// BANNERS MODAL 
+
+var bannerModal = document.getElementById('bannerModal');
+var bannerBtn = document.getElementById('banner-modal-btn');
+var close = document.getElementsByClassName('close')[0];
+
+bannerBtn.onclick = function () {
+    bannerModal.style.display = "block";
+}
+
+close.onclick = function () {
+    bannerModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == bannerModal) {
+        bannerModal.style.display = "none";
+    }
 }
