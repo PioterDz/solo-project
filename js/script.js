c3cddd64a24847f51269d6b971e74978809fb4f2
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-03-03 19:14:07 +0100

    fix compiler

--- js/script.js
+++ js/script.js
@@ -11,40 +11,77 @@ document.querySelector('.hamburger').addEventListener('click', function(e) {
 
 // QUIT MODAL
 
-var quitModal = document.getElementById('quitModal');
-var quitBtn = document.getElementById('quitBtn');
-var quitModalBtn = document.getElementById('quitModalBtn');
+var quitPageModal = document.getElementById('quitModal');
+var openQuitPageModalButton = document.getElementById('quitBtn');
+var escapeQuitPageModalButton = document.getElementById('quitModalBtn');
 
-quitBtn.onclick = function () {
-    quitModal.style.display = "block";
-}
+openQuitPageModalButton.addEventListener('click', function () {
+    quitPageModal.style.display = "block";
+})
 
-quitModalBtn.onclick = function () {
-    quitModal.style.display = "none";
-}
+escapeQuitPageModalButton.addEventListener('click', function () {
+    quitPageModal.style.display = "none";
+})
 
-window.onclick = function(event) {
-    if (event.target == quitModal) {
-        quitModal.style.display = "none";
+document.body.addEventListener('click', function(event) {
+    if (event.target == quitPageModal) {
+        quitPageModal.style.display = "none";
     }
-}
+})
 
 // BANNERS MODAL 
 
 var bannerModal = document.getElementById('bannerModal');
-var bannerBtn = document.getElementById('banner-modal-btn');
+var openBannerModalBtn = document.getElementById('bannerModalBtn');
 var close = document.getElementsByClassName('close')[0];
 
-bannerBtn.onclick = function () {
+openBannerModalBtn.addEventListener('click', function () {
+    console.log('banner banner');
     bannerModal.style.display = "block";
-}
+})
 
-close.onclick = function () {
+close.addEventListener('click', function () {
     bannerModal.style.display = "none";
-}
+})
 
-window.onclick = function(event) {
+document.body.addEventListener('click', function(event) {
     if (event.target == bannerModal) {
         bannerModal.style.display = "none";
     }
-}
+})
+
+var canvas = document.getElementById('myChart');
+var ctx = canvas.getContext('2d');
+
+var chart = new Chart(ctx, {
+    // 1
+    type: 'bar',
+    data: {
+        // 2
+        labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+        // 3
+        datasets: [{
+            // 4
+            label: "Signups",
+            // 5
+            backgroundColor: '#8DBEC8',
+            borderColor: '#8DBEC8',
+            // 6
+            data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+        },
+        {
+            label: "FTD",
+            backgroundColor: '#F29E4E',
+            borderColor: '#F29E4E',
+            data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+        },
+        {
+            label: "Earned",
+            backgroundColor: '#71B374',
+            borderColor: '#71B374',
+            data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+            // 7
+            hidden: true,
+        }]
+    },
+});
